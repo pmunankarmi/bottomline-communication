5adb5fbfd4c3a1a75d534838d6bdb9f915c8cd57
Puskar <puskar@Puskars-MacBook-Pro.local>
2026-09-23 20:04:04 +0545

    Clean client seed data formatting

--- bottomline-communication/inc/default-clients.php
+++ bottomline-communication/inc/default-clients.php
@@ -2,80 +2,80 @@
 /** Original client sectors and gallery order. */
 defined( 'ABSPATH' ) || exit;
 return array (
-  0 => 
+  0 =>
   array (
     'title' => 'Government & Enterprise',
     'placement' => 'clients',
-    'images' => 
+    'images' =>
     array (
-      0 => 
+      0 =>
       array (
         'path' => 'clients/saudi-aramco.png',
         'alt' => 'Saudi Aramco',
       ),
-      1 => 
+      1 =>
       array (
         'path' => 'clients/sme-bank.png',
         'alt' => 'SME Bank',
       ),
-      2 => 
+      2 =>
       array (
         'path' => 'clients/ministry-of-health.png',
         'alt' => 'Ministry of Health',
       ),
-      3 => 
+      3 =>
       array (
         'path' => 'clients/mbsc.png',
         'alt' => 'MBSC',
       ),
-      4 => 
+      4 =>
       array (
         'path' => 'clients/go-telecom.png',
         'alt' => 'GO Telecom',
       ),
-      5 => 
+      5 =>
       array (
         'path' => 'clients/boston-scientific.png',
         'alt' => 'Boston Scientific',
       ),
-      6 => 
+      6 =>
       array (
         'path' => 'clients/sarcc.png',
         'alt' => 'SARCC',
       ),
-      7 => 
+      7 =>
       array (
         'path' => 'clients/alkhorayef.png',
         'alt' => 'Al Khorayef',
       ),
-      8 => 
+      8 =>
       array (
         'path' => 'clients/rawasy-investment.png',
         'alt' => 'Rawasy Investment',
       ),
-      9 => 
+      9 =>
       array (
         'path' => 'clients/altamayyuz.png',
         'alt' => 'Altamayyuz',
       ),
     ),
-    'text_logos' => 
+    'text_logos' =>
     array (
     ),
   ),
-  1 => 
+  1 =>
   array (
     'title' => 'Sports & Lifestyle',
     'placement' => 'clients',
-    'images' => 
+    'images' =>
     array (
-      0 => 
+      0 =>
       array (
         'path' => 'clients/sela.png',
         'alt' => 'Sela',
       ),
     ),
-    'text_logos' => 
+    'text_logos' =>
     array (
       0 => '2: UFC',
       1 => '3: CR7',
@@ -83,427 +83,427 @@ return array (
       3 => '5: 5 vs 5',
     ),
   ),
-  2 => 
+  2 =>
   array (
     'title' => 'Automotive',
     'placement' => 'clients',
-    'images' => 
+    'images' =>
     array (
-      0 => 
+      0 =>
       array (
         'path' => 'clients/bmw.png',
         'alt' => 'BMW',
       ),
-      1 => 
+      1 =>
       array (
         'path' => 'clients/ford.png',
         'alt' => 'Ford',
       ),
-      2 => 
+      2 =>
       array (
         'path' => 'clients/mini.png',
         'alt' => 'MINI Cooper',
       ),
-      3 => 
+      3 =>
       array (
         'path' => 'clients/naghi-motors.png',
         'alt' => 'Naghi Motors',
       ),
-      4 => 
+      4 =>
       array (
         'path' => 'clients/dms.png',
         'alt' => 'DMS',
       ),
     ),
-    'text_logos' => 
+    'text_logos' =>
     array (
     ),
   ),
-  3 => 
+  3 =>
   array (
     'title' => 'Hospitality',
     'placement' => 'clients',
-    'images' => 
+    'images' =>
     array (
-      0 => 
+      0 =>
       array (
         'path' => 'clients/radisson-blu.png',
         'alt' => 'Radisson Blu',
       ),
-      1 => 
+      1 =>
       array (
         'path' => 'clients/crowne-plaza.png',
         'alt' => 'Crowne Plaza',
       ),
-      2 => 
+      2 =>
       array (
         'path' => 'clients/belajio.png',
         'alt' => 'Belajio Resort',
       ),
-      3 => 
+      3 =>
       array (
         'path' => 'clients/stars-avenue.png',
         'alt' => 'Stars Avenue',
       ),
     ),
-    'text_logos' => 
+    'text_logos' =>
     array (
       0 => '5: Sushi Library',
     ),
   ),
-  4 => 
+  4 =>
   array (
     'title' => 'Retail & Fashion',
     'placement' => 'clients',
-    'images' => 
+    'images' =>
     array (
-      0 => 
+      0 =>
       array (
         'path' => 'clients/landmark-group.png',
         'alt' => 'Landmark Group',
       ),
-      1 => 
+      1 =>
       array (
         'path' => 'clients/sjp.png',
         'alt' => 'SJP',
       ),
-      2 => 
+      2 =>
       array (
         'path' => 'clients/papabubble.png',
         'alt' => 'Papabubble',
       ),
     ),
-    'text_logos' => 
+    'text_logos' =>
     array (
       0 => '2: Centrepoint',
       1 => '4: Femi 9',
     ),
   ),
-  5 => 
+  5 =>
   array (
     'title' => 'Food & Beverage',
     'placement' => 'clients',
-    'images' => 
+    'images' =>
     array (
-      0 => 
+      0 =>
       array (
         'path' => 'clients/three-olives.png',
         'alt' => 'Three Olives',
       ),
-      1 => 
+      1 =>
       array (
         'path' => 'clients/nestle-toll-house.png',
         'alt' => 'Nestlé Toll House',
       ),
-      2 => 
+      2 =>
       array (
         'path' => 'clients/pizza-fusion.png',
         'alt' => 'Pizza Fusion',
       ),
-      3 => 
+      3 =>
       array (
         'path' => 'clients/tokana.png',
         'alt' => 'Tokana',
       ),
-      4 => 
+      4 =>
       array (
         'path' => 'clients/tonofa.png',
         'alt' => 'Tonofa',
       ),
     ),
-    'text_logos' => 
+    'text_logos' =>
     array (
     ),
   ),
-  6 => 
+  6 =>
   array (
     'title' => 'Entertainment & Media',
     'placement' => 'clients',
-    'images' => 
+    'images' =>
     array (
-      0 => 
+      0 =>
       array (
         'path' => 'clients/grand-cinemas.png',
         'alt' => 'Grand Cinemas',
       ),
-      1 => 
+      1 =>
       array (
         'path' => 'clients/empire-cinemas.png',
         'alt' => 'Empire Cinemas',
       ),
-      2 => 
+      2 =>
       array (
         'path' => 'clients/gazzaz.png',
         'alt' => 'Gazzaz Production',
       ),
     ),
-    'text_logos' => 
+    'text_logos' =>
     array (
       0 => '4: Rekab',
       1 => '5: Nashir',
     ),
   ),
-  7 => 
+  7 =>
   array (
     'title' => 'Education',
     'placement' => 'clients',
-    'images' => 
+    'images' =>
     array (
-      0 => 
+      0 =>
       array (
         'path' => 'clients/jeddah-campus.png',
         'alt' => 'Jeddah Campus',
       ),
-      1 => 
+      1 =>
       array (
         'path' => 'clients/madares-al-marefa.png',
         'alt' => 'Madares Al Marefa',
       ),
     ),
-    'text_logos' => 
+    'text_logos' =>
     array (
     ),
   ),
-  8 => 
+  8 =>
   array (
     'title' => 'Industrial & B2B',
     'placement' => 'clients',
-    'images' => 
+    'images' =>
     array (
-      0 => 
+      0 =>
       array (
         'path' => 'clients/haier.png',
         'alt' => 'Haier',
       ),
-      1 => 
+      1 =>
       array (
         'path' => 'clients/unicoil.png',
         'alt' => 'Unicoil',
       ),
-      2 => 
+      2 =>
       array (
         'path' => 'clients/neproplast.png',
         'alt' => 'Neproplast',
       ),
-      3 => 
+      3 =>
       array (
         'path' => 'clients/gulf-bridge.png',
         'alt' => 'Gulf Bridge',
       ),
-      4 => 
+      4 =>
       array (
         'path' => 'clients/asamco.png',
         'alt' => 'ASAMCO Almarbaie',
       ),
-      5 => 
+      5 =>
       array (
         'path' => 'clients/fmc.png',
         'alt' => 'FMC Construction',
       ),
-      6 => 
+      6 =>
       array (
         'path' => 'clients/jcc.png',
         'alt' => 'JCC Contracting',
       ),
-      7 => 
+      7 =>
       array (
         'path' => 'clients/first-fix.png',
         'alt' => 'First Fix',
       ),
-      8 => 
+      8 =>
       array (
         'path' => 'clients/al-haddad.png',
         'alt' => 'Al-Haddad Telecom',
       ),
     ),
-    'text_logos' => 
+    'text_logos' =>
     array (
     ),
   ),
-  9 => 
+  9 =>
   array (
     'title' => 'Services & Tech',
     'placement' => 'clients',
-    'images' => 
+    'images' =>
     array (
-      0 => 
+      0 =>
       array (
         'path' => 'clients/pointsgram.png',
         'alt' => 'Pointsgram',
       ),
-      1 => 
+      1 =>
       array (
         'path' => 'clients/briefill.png',
         'alt' => 'Briefill',
       ),
-      2 => 
+      2 =>
       array (
         'path' => 'clients/bridges.png',
         'alt' => 'Bridges Advertising',
       ),
-      3 => 
+      3 =>
       array (
         'path' => 'clients/optimal-concept.png',
         'alt' => 'Optimal Concept',
       ),
-      4 => 
+      4 =>
       array (
         'path' => 'clients/amid-consulting.png',
         'alt' => 'Amid Consulting',
       ),
-      5 => 
+      5 =>
       array (
         'path' => 'clients/ops.png',
         'alt' => 'OPS',
       ),
-      6 => 
+      6 =>
       array (
         'path' => 'clients/fastmile.png',
         'alt' => 'Fastmile',
       ),
-      7 => 
+      7 =>
       array (
         'path' => 'clients/baraya.png',
         'alt' => 'Baraya',
       ),
-      8 => 
+      8 =>
       array (
         'path' => 'clients/athr.png',
         'alt' => 'ATHR',
       ),
     ),
-    'text_logos' => 
+    'text_logos' =>
     array (
       0 => '10: Finest Business',
     ),
   ),
-  10 => 
+  10 =>
   array (
     'title' => 'Homepage logos',
     'placement' => 'home',
-    'images' => 
+    'images' =>
     array (
-      0 => 
+      0 =>
       array (
         'path' => 'clients/saudi-aramco.png',
         'alt' => 'Saudi Aramco',
       ),
-      1 => 
+      1 =>
       array (
         'path' => 'clients/sme-bank.png',
         'alt' => 'SME Bank',
       ),
-      2 => 
+      2 =>
       array (
         'path' => 'clients/ministry-of-health.png',
         'alt' => 'Ministry of Health',
       ),
-      3 => 
+      3 =>
       array (
         'path' => 'clients/mbsc.png',
         'alt' => 'MBSC',
       ),
-      4 => 
+      4 =>
       array (
         'path' => 'clients/go-telecom.png',
         'alt' => 'GO Telecom',
       ),
-      5 => 
+      5 =>
       array (
         'path' => 'clients/boston-scientific.png',
         'alt' => 'Boston Scientific',
       ),
-      6 => 
+      6 =>
       array (
         'path' => 'clients/sarcc.png',
         'alt' => 'SARCC',
       ),
-      7 => 
+      7 =>
       array (
         'path' => 'clients/alkhorayef.png',
         'alt' => 'Al Khorayef',
       ),
-      8 => 
+      8 =>
       array (
         'path' => 'clients/altamayyuz.png',
         'alt' => 'Altamayyuz',
       ),
-      9 => 
+      9 =>
       array (
         'path' => 'clients/sela.png',
         'alt' => 'Sela',
       ),
-      10 => 
+      10 =>
       array (
         'path' => 'clients/rawasy-investment.png',
         'alt' => 'Rawasy Investment',
       ),
-      11 => 
+      11 =>
       array (
         'path' => 'clients/naghi-motors.png',
         'alt' => 'Naghi Motors',
       ),
-      12 => 
+      12 =>
       array (
         'path' => 'clients/bmw.png',
         'alt' => 'BMW',
       ),
-      13 => 
+      13 =>
       array (
         'path' => 'clients/ford.png',
         'alt' => 'Ford',
       ),
-      14 => 
+      14 =>
       array (
         'path' => 'clients/mini.png',
         'alt' => 'MINI',
       ),
-      15 => 
+      15 =>
       array (
         'path' => 'clients/radisson-blu.png',
         'alt' => 'Radisson Blu',
       ),
-      16 => 
+      16 =>
       array (
         'path' => 'clients/crowne-plaza.png',
         'alt' => 'Crowne Plaza',
       ),
-      17 => 
+      17 =>
       array (
         'path' => 'clients/landmark-group.png',
         'alt' => 'Landmark Group',
       ),
-      18 => 
+      18 =>
       array (
         'path' => 'clients/sjp.png',
         'alt' => 'SJP',
       ),
-      19 => 
+      19 =>
       array (
         'path' => 'clients/briefill.png',
         'alt' => 'Briefill',
       ),
-      20 => 
+      20 =>
       array (
         'path' => 'clients/grand-cinemas.png',
         'alt' => 'Grand Cinemas',
       ),
-      21 => 
+      21 =>
       array (
         'path' => 'clients/empire-cinemas.png',
         'alt' => 'Empire Cinemas',
       ),
-      22 => 
+      22 =>
       array (
         'path' => 'clients/haier.png',
         'alt' => 'Haier',
       ),
-      23 => 
+      23 =>
       array (
         'path' => 'clients/nestle-toll-house.png',
         'alt' => 'Nestlé Toll House',
       ),
-      24 => 
+      24 =>
       array (
         'path' => 'clients/pointsgram.png',
         'alt' => 'Pointsgram',
